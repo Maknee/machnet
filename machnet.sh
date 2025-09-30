b5178fbcba38efd5dcb408ebdb8de9bcf6f8c377
maknee <zhu109@illinois.edu>
2025-09-30 18:08:52 +0000

    A

--- machnet.sh
+++ machnet.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # Start the Machnet service on this machineA
-# Usage: machnet.sh --mac <local MAC> --ip <local IP>
+# Usage: machnet.sh --mac <local MAC> --ip <local IP> [--engine_threads <threads>] [--num_huge_pages <pages>]
 #  - mac: MAC address of the local DPDK interface
 #  - ip: IP address of the local DPDK interface
+#  - engine_threads: Number of engine threads (default: 1)
+#  - num_huge_pages: Number of 2MB hugepages to allocate (default: 1024)
 #  - debug: if set, run a debug build from the Machnet Docker container
 #  - bare_metal: if set, will use local binary instead of Docker image
 
 LOCAL_MAC=""
 LOCAL_IP=""
+ENGINE_THREADS=1
+NUM_HUGE_PAGES=1024
 BARE_METAL=0
 DEBUG=0
 while [[ $# -gt 0 ]]; do
@@ -20,6 +24,16 @@ while [[ $# -gt 0 ]]; do
             ;;
         -i|--ip)
             LOCAL_IP="$2"
+            shift
+            shift
+            ;;
+        -e|--engine_threads)
+            ENGINE_THREADS="$2"
+            shift
+            shift
+            ;;
+        -n|--num_huge_pages)
+            NUM_HUGE_PAGES="$2"
             shift
             shift
             ;;
@@ -40,7 +54,7 @@ done
 
 # Pre-flight checks
 if [ -z "$LOCAL_MAC" ] || [ -z "$LOCAL_IP" ]; then
-    echo "Usage: machnet.sh --mac <local MAC> --ip <local IP>"
+    echo "Usage: machnet.sh --mac <local MAC> --ip <local IP> [--engine_threads <threads>] [--num_huge_pages <pages>]"
     exit 1
 fi
 
@@ -49,23 +63,23 @@ fi
 #
 
 #Allocate memory for the first NUMA node
-if ! cat /sys/devices/system/node/*/meminfo | grep HugePages_Total | grep -q 1024
+if ! cat /sys/devices/system/node/*/meminfo | grep HugePages_Total | grep -q $NUM_HUGE_PAGES
 then
     echo "Insufficient or no hugepages available"
-    read -p "Do you want to allocate 1024 2MB hugepages? (y/n) " -n 1 -r
+    read -p "Do you want to allocate $NUM_HUGE_PAGES 2MB hugepages? (y/n) " -n 1 -r
     echo
     if [[ ! $REPLY =~ ^[Yy]$ ]]
     then
         echo "OK, continuing without allocating hugepages"
     else
-        echo "Allocating 1024 hugepages"
-        sudo bash -c "echo 1024 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
-        if ! cat /sys/devices/system/node/*/meminfo | grep HugePages_Total | grep -q 1024
+        echo "Allocating $NUM_HUGE_PAGES hugepages"
+        sudo bash -c "echo $NUM_HUGE_PAGES > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
+        if ! cat /sys/devices/system/node/*/meminfo | grep HugePages_Total | grep -q $NUM_HUGE_PAGES
         then
             echo "Failed to allocate hugepages"
             exit 1
         else
-            echo "Successfully allocated 1024 hugepages on NUMA node0"
+            echo "Successfully allocated $NUM_HUGE_PAGES hugepages on NUMA node0"
         fi
     fi
 fi
@@ -73,13 +87,13 @@ fi
 # Allocate memory for the rest of the NUMA nodes, if any
 for n in /sys/devices/system/node/node[1-9]; do
     if [ -d "$n" ]; then
-        sudo bash -c "echo 1024 > $n/hugepages/hugepages-2048kB/nr_hugepages"
-        if ! cat $n/meminfo | grep HugePages_Total | grep -q 1024
+        sudo bash -c "echo $NUM_HUGE_PAGES > $n/hugepages/hugepages-2048kB/nr_hugepages"
+        if ! cat $n/meminfo | grep HugePages_Total | grep -q $NUM_HUGE_PAGES
         then
             echo "Failed to allocate hugepages on NUMA `echo $n | cut -d / -f 6`"
             exit 1
         else
-            echo "Successfully allocated 1024 hugepages on NUMA `echo $n | cut -d / -f 6`"
+            echo "Successfully allocated $NUM_HUGE_PAGES hugepages on NUMA `echo $n | cut -d / -f 6`"
         fi
     fi
 done
@@ -93,7 +107,7 @@ if [ ! -d "/var/run/machnet" ]; then
     sudo chmod 755 /var/run/machnet # Set permissions like Ubuntu's default, needed on (e.g.) CentOS
 fi
 
-sudo bash -c "echo '{\"machnet_config\": {\"$LOCAL_MAC\": {\"ip\": \"$LOCAL_IP\"}}}' > /var/run/machnet/local_config.json"
+sudo bash -c "echo '{\"machnet_config\": {\"$LOCAL_MAC\": {\"ip\": \"$LOCAL_IP\", \"engine_threads\": $ENGINE_THREADS}}}' > /var/run/machnet/local_config.json"
 echo "Created config for local Machnet, in /var/run/machnet/local_config.json. Contents:"
 sudo cat /var/run/machnet/local_config.json
 
